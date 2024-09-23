18e8442d3c00acfafcb0ec5fc0921300037f6e3e
Alexanderito <alexanderpalacios031@gmail.com>
2024-09-22 23:44:27 -0600

    Update procesar_usuario.php

--- procesar_usuario.php
+++ procesar_usuario.php
@@ -1,4 +1,9 @@
 <?php 
+<?php 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Establecer conexión a la base de datos
 $server = "localhost";
 $username_db = "root";
